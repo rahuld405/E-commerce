33914d6ca8cd8e3974cf0a26e7b0b5b22ece1791
Rahul Dhawgade <70699204+rahuld405@users.noreply.github.com>
2023-12-07 18:46:43 +0530

    Update connection.php

--- utility/connection.php
+++ utility/connection.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con= mysqli_connect('ecommerce-db-devtron-demo-service','root','passwd');
+$con= mysqli_connect('ecommerce-db-devtron-demo-service','root','passwd','3306');
 mysqli_select_db($con,'grocerry');
 define('D',"/backend_projects/grocerry");
 ?>
